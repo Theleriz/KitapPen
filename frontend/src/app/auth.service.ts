53c92bdc0b340c3886e423e468db7f9ce57b346b
bakytzhananel <an_bakytzhan@kbtu.kz>
2026-04-21 18:54:40 +0500

    Change API URL from 127.0.0.1 to localhost

--- frontend/src/app/auth.service.ts
+++ frontend/src/app/auth.service.ts
@@ -5,7 +5,8 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root',
 })
 export class AuthService {
-  private apiUrl = 'http://127.0.0.1:8000/api';
+  private apiUrl = 'http://localhost:8000/api';
+
 
   constructor(private http: HttpClient) {}
 
